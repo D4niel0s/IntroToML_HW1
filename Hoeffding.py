2d24b042dd0591771302f3f79322db066fcea448
D4niel0s <das.volkov@gmail.com>
2024-06-11 01:35:16 +0300

    Added legend, some optimizations

--- Hoeffding.py
+++ Hoeffding.py
@@ -9,20 +9,20 @@ def main():
 
     S = np.zeros(N)
     for i in range(N):
-        S[i] = np.sum(mat[i])
-        S[i] = S[i]/float(n)
+        S[i] = np.mean(mat[i])
     
     epsilons = np.linspace(0,1, 50)
     emp_prob = np.zeros(50)
     for i in range(50):
         for j in range(N):
-            if(abs(S[j] - 0.5) > epsilons[i]):
+            if(S[j]-0.5>epsilons[i] or 0.5-S[j]>epsilons[i]):
                 emp_prob[i] += 1
         emp_prob[i] /= float(N)
     
-    hoef = ((-2)*np.power(epsilons, np.full(50,2)))
-    plt.plot(epsilons, emp_prob, 'r')
-    plt.plot(epsilons, hoef, 'b')
+    hoef = 2*np.exp((-2*n)*np.power(epsilons,2))
+    plt.plot(epsilons, hoef, 'b',label='Hoeffding bound')
+    plt.plot(epsilons, emp_prob, 'r', label='Empirical probability')
+    plt.legend()
     plt.show()
 
 
